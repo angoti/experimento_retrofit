c21d26608fb2bb94966df8a42eca3d6e50648161
Edson Angoti Junior <angoti@gmail.com>
2018-11-29 21:07:00 -0200

    Revert "Initial commit"
    This reverts commit 68941fc

---
+++ app/src/main/java/com/example/jorda/experimentoretrofit/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.jorda.experimentoretrofit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jorda.experimentoretrofit.domain.Modelo;
+import com.example.jorda.experimentoretrofit.service.ModeloApi;
+import com.google.gson.Gson;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mUserIdTextView;
+    private TextView mIdTextView;
+    private TextView mTitleTextView;
+    private TextView mBodyTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        Call<Modelo> call = getModeloCall(1);
+
+        call.enqueue(new Callback<Modelo>() {
+            @Override
+            public void onResponse(Call<Modelo> call, Response<Modelo> response) {
+                    Modelo obj = response.body();
+                    Log.i("teste","O titulo cujo id é 1 = "+obj.getTitle());
+                    mTitleTextView.setText(obj.getTitle());
+            }
+
+            @Override
+            public void onFailure(Call<Modelo> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Call<List<Modelo>> call2 = getModeloCall2();
+
+        call2.enqueue(new Callback<List<Modelo>>() {
+            @Override
+            public void onResponse(Call<List<Modelo>> call, Response<List<Modelo>> response) {
+                List<Modelo> lista = response.body();
+                Log.i("teste","tamanho da lista = "+lista.size());
+            }
+
+            @Override
+            public void onFailure(Call<List<Modelo>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private Call<Modelo> getModeloCall(int i) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+
+        ModeloApi apiService = retrofit.create(ModeloApi.class);
+        return apiService.getModelo(i);
+    }
+
+    private Call<List<Modelo>> getModeloCall2() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+
+        ModeloApi apiService = retrofit.create(ModeloApi.class);
+        return apiService.getTodosModelo();
+    }
+
+    private void initViews() {
+        mUserIdTextView = findViewById(R.id.user_id);
+        mIdTextView = findViewById(R.id.id);
+        mTitleTextView = findViewById(R.id.title);
+        mBodyTextView = findViewById(R.id.body);
+    }
+
+    private void writeViewData(Modelo modelo) {
+        mUserIdTextView.setText(modelo.getUserId().toString());
+        mIdTextView.setText(Integer.toString(modelo.getId()));
+        mBodyTextView.setText(modelo.getBody());
+        mTitleTextView.setText(modelo.getTitle());
+    }
+
+
+}
---
+++ app/src/main/java/com/example/jorda/experimentoretrofit/service/ModeloApi.java
@@ -0,0 +1,27 @@
+package com.example.jorda.experimentoretrofit.service;
+
+import com.example.jorda.experimentoretrofit.domain.Modelo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by gabrielgrs
+ * Date: 27/08/2018
+ * Time: 7:34 PM
+ * Project: Retrofittesteaula
+ * Alterated by jordanavilela
+ */
+public interface ModeloApi {
+
+    @GET("/posts/{id}")
+    Call<Modelo> getModelo(@Path("id") int postId);
+
+    @GET("/posts")
+    Call<List<Modelo>> getTodosModelo();
+
+
+}
